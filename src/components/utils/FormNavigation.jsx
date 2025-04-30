a14f383df53c6655a25f8fca2d6291fdb1e17ba5
Pegazar <yusif.feyzullazade@gmail.com>
2025-04-30 13:52:47 +0400

    update

--- src/components/utils/FormNavigation.jsx
+++ src/components/utils/FormNavigation.jsx
@@ -8,7 +8,7 @@ const FormNavigation = ({
   onSubmit,
 }) => {
   return (
-    <div className="flex justify-between items-center relative lg:absolute bottom-0 left-0 right-0 lg:left-22 lg:right-22">
+    <div className="flex justify-between items-center fixed lg:absolute bottom-0 left-0 right-0 lg:left-22 lg:right-22">
       {stepIndex > 0 ? (
         <div>
           <button
